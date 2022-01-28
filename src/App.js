726d5a8e05e4bb2251ac4d40c4be67f2335f24c0
BarbaraRocha <barbaraluizasr@gmail.com>
2022-01-28 16:29:03 -0300

    ate o requisito 5

--- src/App.js
+++ src/App.js
@@ -1,13 +1,75 @@
 import React from 'react';
+import Form from './components/Form';
+import Card from './components/Card';
+import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      description: '',
+      attr1: 0,
+      attr2: 0,
+      attr3: 0,
+      image: '',
+      rare: '',
+      trunfo: false,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name } = target;
+    const value = (target.type === 'checkbox') ? target.checked : target.value;
+    this.setState({ [name]: value });
+  }
+
   render() {
+    const { name, description, attr1, attr2, attr3, image, rare, trunfo } = this.state;
+    const maxNumber = (atributo) => {
+      const maxCards = 90;
+      if (atributo <= maxCards && atributo >= 0) return atributo;
+    };
+
+    const buttonFunc = () => {
+      const firstCond = [name, description, image, rare];
+      const secondCond = [Number(attr1), Number(attr2), Number(attr3)];
+      const verifyButton1 = firstCond.every((condition) => condition.length !== 0);
+      const verifyButton2 = secondCond.reduce((valor, acc) => valor + acc, 0);
+      const maxSumNumber = 210;
+      const button = !(verifyButton1 && verifyButton2 <= maxSumNumber
+        && maxNumber(attr1) && maxNumber(attr2) && maxNumber(attr3));
+      return button;
+    };
+
     return (
-      <div>
+      <main>
         <h1>Tryunfo</h1>
-      </div>
+        <Form
+          cardName={ name }
+          cardDescription={ description }
+          cardAttr1={ attr1 }
+          cardAttr2={ attr2 }
+          cardAttr3={ attr3 }
+          cardImage={ image }
+          cardRare={ rare }
+          cardTrunfo={ trunfo }
+          onInputChange={ this.handleChange }
+          isSaveButtonDisabled={ buttonFunc() }
+          // onSaveButtonClick = { handleButtonClick() }
+        />
+        <Card
+          cardName={ name }
+          cardDescription={ description }
+          cardAttr1={ attr1 }
+          cardAttr2={ attr2 }
+          cardAttr3={ attr3 }
+          cardImage={ image }
+          cardRare={ rare }
+          cardTrunfo={ trunfo }
+        />
+      </main>
     );
   }
 }
-
 export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,147 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Form extends React.Component {
+  render() {
+    const { cardName,
+      cardDescription,
+      cardAttr1, cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo, /* hasTrunfo */
+      isSaveButtonDisabled,
+      onInputChange,
+      onSaveButtonClick } = this.props;
+
+    return (
+      <form id="form">
+        <label htmlFor="name">
+          Nome:
+          <input
+            data-testid="name-input"
+            name="name"
+            onChange={ onInputChange }
+            value={ cardName }
+            type="text"
+            required
+          />
+        </label>
+
+        <label htmlFor="description">
+          Descrição:
+          <input
+            data-testid="description-input"
+            name="description"
+            onChange={ onInputChange }
+            value={ cardDescription }
+            type="textarea"
+            required
+          />
+        </label>
+
+        <label htmlFor="attr1">
+          Attr01:
+          <input
+            data-testid="attr1-input"
+            min="0"
+            max="90"
+            name="attr1"
+            onChange={ onInputChange }
+            value={ cardAttr1 }
+            type="number"
+            required
+          />
+        </label>
+
+        <label htmlFor="attr2">
+          Attr02:
+          <input
+            data-testid="attr2-input"
+            name="attr2"
+            onChange={ onInputChange }
+            value={ cardAttr2 }
+            type="number"
+            required
+          />
+        </label>
+
+        <label htmlFor="attr3">
+          Attr03:
+          <input
+            data-testid="attr3-input"
+            min="0"
+            max="90"
+            name="attr3"
+            onChange={ onInputChange }
+            value={ cardAttr3 }
+            type="number"
+            required
+          />
+        </label>
+
+        <label htmlFor="image">
+          Imagem:
+          <input
+            data-testid="image-input"
+            name="image"
+            onChange={ onInputChange }
+            value={ cardImage }
+            type="text"
+            required
+          />
+        </label>
+
+        <label htmlFor="rare">
+          Raridade:
+          <select
+            data-testid="rare-input"
+            name="rare"
+            onChange={ onInputChange }
+            value={ cardRare }
+            required
+          >
+            <option>normal</option>
+            <option>raro</option>
+            <option>muito raro</option>
+          </select>
+        </label>
+
+        <label htmlFor="trunfo">
+          Super Trybe Trunfo
+          <input
+            data-testid="trunfo-input"
+            name="trunfo"
+            onChange={ onInputChange }
+            checked={ cardTrunfo }
+            type="checkbox"
+          />
+        </label>
+        <button
+          type="submit"
+          data-testid="save-button"
+          disabled={ isSaveButtonDisabled }
+          onClick={ onSaveButtonClick }
+        >
+          Salvar
+        </button>
+      </form>
+    );
+  }
+}
+
+Form.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.number.isRequired,
+  cardAttr2: PropTypes.number.isRequired,
+  cardAttr3: PropTypes.number.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+  isSaveButtonDisabled: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onSaveButtonClick: PropTypes.func.isRequired,
+};
+
+export default Form;
